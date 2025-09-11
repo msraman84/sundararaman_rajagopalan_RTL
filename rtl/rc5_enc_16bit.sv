5be5b557e53e2645635024a1ee7ad1e517e91ea7
unknown <sundararaman.r@turing.com>
2025-09-11 09:00:28 +0530

    Updated rtl directory

--- rtl/rc5_enc_16bit.sv
+++ rtl/rc5_enc_16bit.sv
@@ -6,7 +6,7 @@ module rc5_enc_16bit(input clock,reset,enc_start, input [15:0]p, output reg [15:
 	reg [2:0] state;//State representation to manage operations of encryption algorithm FSM
 	assign s[0] = 8'h20;//S-box variables assignment
 	assign s[1] = 8'h10;
-	assign s[2] = 8'h0F;
+	assign s[2] = 8'hFF;
 	assign s[3] = 8'hFF;
 	always_ff @(posedge clock)//Encryption is carried out during positive clock edges of clock input
 	begin
@@ -22,12 +22,12 @@ module rc5_enc_16bit(input clock,reset,enc_start, input [15:0]p, output reg [15:
 			begin
 				case (state)
 				3'b000:	begin //Initial addition stage
-						p_tmp[15:8] <= (p_tmp[15:8] + s[0]) % 9'h100;
+						p_tmp[15:8] <= (p_tmp[15:8] - s[0]) % 9'h100;
 						p_tmp[7:0] <= (p_tmp[7:0] + s[1]) % 9'h100;
 						state <= 3'b001;
 						end
 				3'b001:	begin // Computation of MSB 8-bits
-						p_tmp[15:8] <= ((((p_tmp[15:8] ^ p_tmp[7:0]) << (p_tmp[7:0]%8)) | ((p_tmp[15:8] ^ p_tmp[7:0]) >> (8 - (p_tmp[7:0]%8)))) + s[2]) % 9'h100;
+						p_tmp[15:8] <= ((((p_tmp[15:8] & p_tmp[7:0]) << (p_tmp[7:0]%8)) | ((p_tmp[15:8] ^ p_tmp[7:0]) >> (8 - (p_tmp[7:0]%8)))) + s[2]) % 9'h100;
 						state <= 3'b010;
 						end
 				3'b010:	begin //Computation of LSB 8-bits
