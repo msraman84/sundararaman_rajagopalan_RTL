40fd3b9d2f66bb500ef47be8e87d49a0239fb2b6
msraman84 <31140418+msraman84@users.noreply.github.com>
2025-09-22 02:21:42 +0530

    Update test_rc5_enc_16bit.py

--- harness/test_rc5_enc_16bit.py
+++ harness/test_rc5_enc_16bit.py
@@ -33,13 +33,13 @@ async def test_encryption_2(dut):
 
     await cocotb.start(generate_clock(dut))  # run the clock "in the background"
     print("RC5 encryption");
-    await Timer(5, units="ns")  # wait a bit
+    await Timer(5, units="ns")  
     dut.reset.value = 0;
     dut.enc_start.value = 1;
     dut.p.value = 0xFFFF;
-    await Timer(15, units="ns")  # wait a bit
+    await Timer(15, units="ns")  #wait for reset
     dut.reset.value = 1;
-    await Timer(150, units="ns")  # wait a bit
+    await Timer(150, units="ns")  
     dut._log.info("reset = %d, enc_start = %d, p = %x, c = %x, enc_done = %d",dut.reset.value,dut.enc_start.value,dut.p.value,dut.c.value,dut.enc_done.value)
     assert dut.c.value == 0x58CB, "Ciphertext is not correct"   
     
@@ -49,13 +49,13 @@ async def test_encryption_3(dut):
 
     await cocotb.start(generate_clock(dut))  # run the clock "in the background"
     print("RC5 encryption");
-    await Timer(5, units="ns")  # wait a bit
+    await Timer(5, units="ns")  
     dut.reset.value = 0;
     dut.enc_start.value = 1;
     dut.p.value = 0x00FF;
-    await Timer(15, units="ns")  # wait a bit
+    await Timer(15, units="ns")  #wait for reset
     dut.reset.value = 1;
-    await Timer(150, units="ns")  # wait a bit
+    await Timer(150, units="ns")  
     dut._log.info("reset = %d, enc_start = %d, p = %x, c = %x, enc_done = %d",dut.reset.value,dut.enc_start.value,dut.p.value,dut.c.value,dut.enc_done.value)
     assert dut.c.value == 0xAFE8, "Ciphertext is not correct"   
     
@@ -65,13 +65,13 @@ async def test_encryption_4(dut):
 
     await cocotb.start(generate_clock(dut))  # run the clock "in the background"
     print("RC5 encryption");
-    await Timer(5, units="ns")  # wait a bit
+    await Timer(5, units="ns")  
     dut.reset.value = 0;
     dut.enc_start.value = 1;
     dut.p.value = 0xFF00;
-    await Timer(15, units="ns")  # wait a bit
+    await Timer(15, units="ns")  #wait for reset
     dut.reset.value = 1;
-    await Timer(150, units="ns")  # wait a bit
+    await Timer(150, units="ns")  
     dut._log.info("reset = %d, enc_start = %d, p = %x, c = %x, enc_done = %d",dut.reset.value,dut.enc_start.value,dut.p.value,dut.c.value,dut.enc_done.value)
     assert dut.c.value == 0x86CF, "Ciphertext is not correct"   
     
