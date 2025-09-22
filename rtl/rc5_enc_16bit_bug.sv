088fa96b30d36e475ef20d714ac9ce6846a3a45f
unknown <sundararaman.r@turing.com>
2025-09-23 00:15:16 +0530

    Added rtl directory

--- rtl/rc5_enc_16bit_bug.sv
+++ rtl/rc5_enc_16bit_bug.sv
@@ -1,9 +1,9 @@
-module rc5_enc_16bit(input clock,
-                     input reset,
-					 input enc_start, 
-					 input [15:0]p, 
-					 output reg [15:0]c, 
-					 output reg enc_done);
+module rc5_enc_16bit_bug(input clock,
+                         input reset,
+					     input enc_start, 
+					     input [15:0]p, 
+					     output reg [15:0]c, 
+					     output reg enc_done);
 	logic [7:0]s[0:3];
 	reg [15:0] p_tmp;//Internal signal to handle encryption operation 
 	reg [2:0] state;//State representation to manage operations of encryption algorithm FSM
@@ -30,11 +30,11 @@ module rc5_enc_16bit(input clock,
 						state <= 3'b001;
 						end
 				3'b001:	begin 
-						p_tmp[15:8] <= ((((p_tmp[15:8] ^ p_tmp[7:0]) << (p_tmp[7:0]%8)) | ((p_tmp[15:8] ^ p_tmp[7:0]) >> (8 - (p_tmp[7:0]%8)))) + s[2]) % 9'h100;
+						p_tmp[15:12] <= ((((p_tmp[15:8] ^ p_tmp[7:0]) << (p_tmp[7:0]%4)) | ((p_tmp[15:8] ^ p_tmp[7:0]) >> (4 - (p_tmp[7:0]%4)))) + s[2]) % 9'h10;
 						state <= 3'b010;
 						end
 				3'b010:	begin 
-						p_tmp[7:0] <= ((((p_tmp[7:0] ^ p_tmp[15:8]) << (p_tmp[15:8]%8)) | ((p_tmp[7:0] ^ p_tmp[15:8]) >> (8 - (p_tmp[15:8]%8)))) + s[3]) % 9'h100;
+						p_tmp[7:4] <= ((((p_tmp[7:0] ^ p_tmp[15:8]) << (p_tmp[15:8]%4)) | ((p_tmp[7:0] ^ p_tmp[15:8]) >> (4 - (p_tmp[15:8]%4)))) + s[3]) % 9'h10;
 						state <= 3'b011;
 						end
 				3'b011:	begin 
