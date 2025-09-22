810b54f4282ae4e1917304aaae7b13aaf6ea17c5
msraman84 <31140418+msraman84@users.noreply.github.com>
2025-09-23 00:37:53 +0530

    Update tb_rc5_enc.sv

--- verif/tb_rc5_enc.sv
+++ verif/tb_rc5_enc.sv
@@ -29,22 +29,17 @@ module tb_rc5_enc();
 	end
 
 	initial begin 
-	    //Test 1
-		rst = 1'b0;enc_start <= 1'b1; enc_p <= 16'h1000;
-		#15 rst <= 1'b1; 
-		#150 rst = 1'b0;
-		
-		//Test 2
+		//Test 1
 		rst = 1'b0;enc_start <= 1'b1; enc_p <= 16'hFFFF;
 		#15 rst <= 1'b1; 
 		#150 rst = 1'b0;
 		
-	    //Test 3
+	    //Test 2
 		rst = 1'b0;enc_start <= 1'b1; enc_p <= 16'h00FF;
 		#15 rst <= 1'b1; 
 		#150 rst = 1'b0;
 		
-		//Test 4
+		//Test 3
 		rst = 1'b0;enc_start <= 1'b1; enc_p <= 16'hFF00;
 		#15 rst <= 1'b1; 
 		#150 rst = 1'b0;
@@ -56,8 +51,19 @@ module tb_rc5_enc();
 	end  
 
 	//Monitor to display the stimulus output of DUT
-	always@(posedge clk)
-		$monitor($time,"enc_start =  %d, enc_p = %x enc_c = %x enc_done = %d",enc_start,enc_p,enc_c,enc_done);
+	always@(negedge clk) begin
+		$monitor($time,"enc_start =  %d, enc_p = %h enc_c = %h enc_done = %d",enc_start,enc_p,enc_c,enc_done);
+        if (enc_done == 1'b1) begin
+		    if ((enc_p == 16'hFFFF) && (enc_c == 16'h0703))
+			    $display("Ciphertext is correct");
+		    else if ((enc_p == 16'hFF00) && (enc_c == 16'h0E86))
+			    $display("Ciphertext is correct");
+		    else if ((enc_p == 16'h00FF) && (enc_c == 16'h9665))
+			    $display("Ciphertext is correct");
+		    else
+			    $display("Ciphertext is not correct");
+		end
+	end
 
 	//TB run time based on number of tests to be executed
 	initial begin
